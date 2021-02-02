7c4eede5a81357530f86fe7447db3a4b8dd84e9d
Or Linzer <orlinzer@gmail.com>
2021-02-02 15:42:48 +0200

    icons

--- develop/index.js
+++ develop/index.js
@@ -1,2 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
-console.log(__dirname);
+console.log('reading index.html');
+
+var index = "";
+
+fs.readFile(path.join(__dirname, '..', 'index.html'), (err, data) => {
+  index += data;
+
+  // TODO read every thing but the main.
+  document.add
+
+  console.log(index);
+});
